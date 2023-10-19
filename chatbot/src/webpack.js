86a63d1661e70d8ed1b7bc7f7ce85d10e8b12d8f
Fabio Zanini <fabio.zanini@fastmail.fm>
2023-10-19 11:48:11 +1100

    Ask and redirect URL works from console in devserver

--- chatbot/src/webpack.js
+++ chatbot/src/webpack.js
@@ -1,11 +1,24 @@
 let { FabilabNlp } = require('./chatbot.js');
 let nlp = new FabilabNlp();
 
+
+const intentCallback = (intent) => {
+  let url = "/pages/" + intent + ".html";
+  window.location.href = url;
+};
+
+
+const askAndRedirect = async (question) => {
+  let response = await chatbot.ask(question);
+  return intentCallback(response.intent);
+};
+
+
 (async () => {
 
   await nlp.initialise();
-
   window.chatbot = nlp;
+  //console.log("Module loaded");
+  window.chatbot.askAndRedirect = askAndRedirect;
 
-  console.log("Module loaded");
 })();
